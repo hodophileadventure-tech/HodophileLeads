683633a60b0dc420a3245522161d3184f4cc1155
mateeque39 <mateeque39@gmail.com>
2026-07-17 20:35:24 +0500

    updates

--- frontend/src/components/AgentPanel.tsx
+++ frontend/src/components/AgentPanel.tsx
@@ -998,6 +998,7 @@ export const AgentPanel: React.FC = () => {
                   requestId={selectedRequest.id}
                   requestType={selectedRequest.requestType}
                   requestStatus={selectedRequest.status as any}
+                  embedded={true}
                   leadData={{
                     clientName: selectedRequest.leadClientName,
                     phone: selectedRequest.leadPhone,
