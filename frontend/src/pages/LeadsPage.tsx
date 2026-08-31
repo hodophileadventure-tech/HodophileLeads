93e405eb2509f24ace5245e48386e895a764ecdb
mateeque39 <mateeque39@gmail.com>
2026-08-31 18:48:21 +0500

    Fix: Add useEffect import to LeadsPage.tsx

--- frontend/src/pages/LeadsPage.tsx
+++ frontend/src/pages/LeadsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { leadsAPI, followUpsAPI } from '../utils/api-service';
 import { Button, Badge, Modal } from '../components/common';
 import { LeadForm } from '../components/LeadForm';
